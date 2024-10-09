12649c3a1561a1f72c6cd7767710548b3999fd38
oskarmatre <69513601+oskarmatre@users.noreply.github.com>
2024-10-09 15:30:33 +1100

    feat: send request from backend to LLM

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const GOOGLE_API_KEY = "AIzaSyA_a8vx9S8BfkyxK4KyhmvemvXa3rhPcBY";
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Load environment variables from .env file
 dotenv.config();
@@ -14,9 +16,24 @@ app.use(cors());
 app.use(express.json());
 
 // Test route
-app.get('/', (req, res) => {
-    res.send('Backend is running!');
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
 });
+
+app.get("/api/exercise", async (req, res) => {
+  const genAI = new GoogleGenerativeAI(GOOGLE_API_KEY);
+
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const prompt = "Give me a suggestion for an exercise.";
+
+  const result = await model.generateContent(prompt);
+  console.log(result.response.text());
+
+  res.send(JSON.stringify(result.response.text()));
+});
+
+
 
 // Start server
 app.listen(PORT, () => {
