dff3d82f8952746941c027a6d08da9517e5f2376
oskarmatre <69513601+oskarmatre@users.noreply.github.com>
2024-10-12 13:42:49 +1100

    feat: create db tablees if they dont exsist on startup

---
+++ backend/db/setuptables.js
@@ -0,0 +1,108 @@
+// function to create mysql tables if they do not exist
+
+const db = require("./connection");
+function setupTables() {
+  db.query(
+    `CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    weight FLOAT NOT NULL,
+    height FLOAT NOT NULL,
+    activity_level VARCHAR(255) NOT NULL
+);`,
+    (err, result) => {
+      if (err) throw err;
+      console.log("Table users created");
+    }
+  );
+  db.query(
+    `CREATE TABLE IF NOT EXISTS meals (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    calories FLOAT NOT NULL,
+    date DATE NOT NULL,
+    budget FLOAT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);`,
+    (err, result) => {
+      if (err) throw err;
+      console.log("Table meals created");
+    }
+  );
+
+  db.query(
+    `CREATE TABLE IF NOT EXISTS ingredients (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        meal_id INT,
+        name VARCHAR(255) NOT NULL,
+        quantity FLOAT NOT NULL,
+        price FLOAT NOT NULL,
+        FOREIGN KEY (meal_id) REFERENCES meals(id)
+    );`,
+    (err, result) => {
+      if (err) throw err;
+      console.log("Table ingredients created");
+    }
+  );
+
+  db.query(
+    `CREATE TABLE IF NOT EXISTS saved_meals (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    meal_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (meal_id) REFERENCES meals(id)
+);`,
+    (err, result) => {
+      if (err) throw err;
+      console.log("Table saved_meals created");
+    }
+  );
+
+  db.query(
+    `CREATE TABLE IF NOT EXISTS exercises (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    exercise_name VARCHAR(255) NOT NULL,
+    calories FLOAT NOT NULL,
+    duration FLOAT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);`,
+    (err, result) => {
+      if (err) throw err;
+      console.log("Table exercises created");
+    }
+  );
+
+  db.query(
+    `CREATE TABLE IF NOT EXISTS saved_exercises (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    exercise_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (exercise_id) REFERENCES exercises(id)
+  );`,
+    (err, result) => {
+      if (err) throw err;
+      console.log("Table saved_exercises created");
+    }
+  );
+
+  db.query(
+    `CREATE TABLE IF NOT EXISTS shopping_list (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    ingredient_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (ingredient_id) REFERENCES ingredients(id)
+);`,
+    (err, result) => {
+      if (err) throw err;
+      console.log("Table shopping_list created");
+    }
+  );
+}
+module.exports = setupTables;
